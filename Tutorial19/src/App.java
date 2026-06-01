51a94e90b6077e72309e3f6ed7302d2c96d26cb1
Kyle Blackburn <kdbburns.za@gmail.com>
2026-06-01 15:48:27 +0200

    Added Java files to Tutorial19 & Eclipse configuration updates.

--- Tutorial19/src/App.java
+++ Tutorial19/src/App.java
@@ -1,9 +1,54 @@
 
+class Thing {
+
+	public final static int LUCKY_NUMBER = 7;
+	public String name;
+	public static String description;
+
+	public static int count = 0;
+
+	public int id;
+
+	public Thing() {
+
+		id = count;
+
+		count++;
+	}
+
+	public void showName() {
+		System.out.println("Object ID:" + id + ", " + description + ": " + name);
+
+	}
+
+	public static void showInfo() {
+		System.out.println(description);
+		// wont work: sysout.println(name);
+	}
+}
+
 public class App {
 
 	public static void main(String[] args) {
 
+		System.out.println("Befor creating objects, count is: " + Thing.count);
 
-	} 
+		Thing thing1 = new Thing();
+		Thing thing2 = new Thing();
+
+		thing1.name = "Bob";
+		thing2.name = "Sue";
+
+		System.out.println("After creating objects, count is: " + Thing.count);
+
+		Thing.description = "I am a anchor";
+
+		thing1.showName();
+		thing2.showName();
+
+		System.out.println((int) Math.PI);
+		System.out.println(Thing.LUCKY_NUMBER);
+
+	}
 
 }
