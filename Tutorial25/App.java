a8dedf0e968447cd99206388d60d9ff35ef7f9ed
iiwiiInsider <163913868+iiwiiInsider@users.noreply.github.com>
2026-06-11 14:06:11 +0200

    Javascripts
    Written via VScode (to note eclipse can handle runtime VScode prints errors)

---
+++ Tutorial25/App.java
@@ -0,0 +1,11 @@
+public class App {
+    
+    public static void main(String[] args) {
+        
+        Plant plant = new Plant();
+        plant.name = "Sunflower";
+        
+        System.out.println(plant.name);
+        
+    }
+}
